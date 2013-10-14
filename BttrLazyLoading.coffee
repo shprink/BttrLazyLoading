ea147ddb7a35b0b41089225038e9ecbc29927524
shprink <julien.renaux@gmail.com>
2013-10-15 00:59:41 +0200

    more basic stuff

--- BttrLazyLoading.coffee
+++ BttrLazyLoading.coffee
@@ -3,37 +3,55 @@ class bttrLazyLoading
 	constructor: (@img, options = {}) ->
 		@$img = $(@img)
 		@loaded = false
-		@dpr = window.devicePixelRatio || 1 
+		@levels = ['xs', 'sm', 'md', 'lg']
 
 		@options = $.extend {
-			original : 
-				width : undefined,
-				height : undefined,
-				src : ''
 			xs :
 				width : 768,
 				src : '',
+				srcRetina : '',
 			sm :
 				width : 768,
 				src : '',
+				srcRetina : '',
 			md :
 				width  : 992,
 				src : '',
+				srcRetina : '',
 			lg :
 				width : 1200,
 				src : '',
-			effect : '',
+				srcRetina : '',
+			effect :
+				type : 'fadeIn',
+				duration : 200
 			event : 'scroll',
-			container : document.body
+			container : window,
+			onBeforeLoad : () ->
+				return
+			onAfterLoad : () ->
+				return
+			threshold : 0,
+			placeholder : 'data:image/gif;base64,R0lGODlhEAALAPQAAP/391tbW+bf3+Da2vHq6l5dXVtbW3h2dq6qqpiVldLMzHBvb4qHh7Ovr5uYmNTOznNxcV1cXI2Kiu7n5+Xf3/fw8H58fOjh4fbv78/JycG8vNzW1vPs7AAAAAAAAAAAACH/C05FVFNDQVBFMi4wAwEAAAAh/hpDcmVhdGVkIHdpdGggYWpheGxvYWQuaW5mbwAh+QQJCwAAACwAAAAAEAALAAAFLSAgjmRpnqSgCuLKAq5AEIM4zDVw03ve27ifDgfkEYe04kDIDC5zrtYKRa2WQgAh+QQJCwAAACwAAAAAEAALAAAFJGBhGAVgnqhpHIeRvsDawqns0qeN5+y967tYLyicBYE7EYkYAgAh+QQJCwAAACwAAAAAEAALAAAFNiAgjothLOOIJAkiGgxjpGKiKMkbz7SN6zIawJcDwIK9W/HISxGBzdHTuBNOmcJVCyoUlk7CEAAh+QQJCwAAACwAAAAAEAALAAAFNSAgjqQIRRFUAo3jNGIkSdHqPI8Tz3V55zuaDacDyIQ+YrBH+hWPzJFzOQQaeavWi7oqnVIhACH5BAkLAAAALAAAAAAQAAsAAAUyICCOZGme1rJY5kRRk7hI0mJSVUXJtF3iOl7tltsBZsNfUegjAY3I5sgFY55KqdX1GgIAIfkECQsAAAAsAAAAABAACwAABTcgII5kaZ4kcV2EqLJipmnZhWGXaOOitm2aXQ4g7P2Ct2ER4AMul00kj5g0Al8tADY2y6C+4FIIACH5BAkLAAAALAAAAAAQAAsAAAUvICCOZGme5ERRk6iy7qpyHCVStA3gNa/7txxwlwv2isSacYUc+l4tADQGQ1mvpBAAIfkECQsAAAAsAAAAABAACwAABS8gII5kaZ7kRFGTqLLuqnIcJVK0DeA1r/u3HHCXC/aKxJpxhRz6Xi0ANAZDWa+kEAA7AAAAAAAAAAAA'
 		}, options
 
-		console.log @options
+		@container = $(@options.container)
+		@dpr = @container.devicePixelRatio || 1 
 
-		@setOriginal({ src: @$img.data 'bttrlazyloading-original' }) if @$img.data 'bttrlazyloading-original'
-		@setXs({ src: @$img.data 'bttrlazyloading-xs'}) if @$img.data 'bttrlazyloading-xs'
-		@setSm({ src: @$img.data 'bttrlazyloading-sm'}) if @$img.data 'bttrlazyloading-sm'
-		@setMd({ src: @$img.data 'bttrlazyloading-md'}) if @$img.data 'bttrlazyloading-md'
-		@setLg({ src: @$img.data 'bttrlazyloading-lg'}) if @$img.data 'bttrlazyloading-lg'
+		# Set options based on Jquery Data available
+		$.each @$img.data(), (i, v) =>
+			if v
+				method = 'set' + i.replace 'bttrlazyloading', ''
+				this[method](v) if typeof this[method] isnt 'undefined'
+
+		#@$img.attr 'src', @options.placeholder					if	!@$img.attr 'src' && @options.placeholder
+		#@$img.css 'width',	@$img.data 'bttrlazyloading-width'	if  @$img.data 'bttrlazyloading-width'
+		#@$img.css 'height',	@$img.data 'bttrlazyloading-height'	if  @$img.data 'bttrlazyloading-height'
+
+		@$img.css
+			'background-image'						: "url('" + @options.placeholder + "')",
+			'background-repeat'						: 'no-repeat',
+			'background-position'					: 'center'
 		
 		@_setupEvents()
 
@@ -42,28 +60,59 @@ class bttrLazyLoading
 	###
 	Private Functions
 	###
-
 	_setupEvents: () ->
 		@$img.bind @options.event, () ->
 			@update()
 
-		@$img.one "appear", ()->
+		@$img.bind 'load', () =>
+			@$img.hide();
+			@$img[@options.effect.type](@options.effect.duration);
+			@loaded = @_getToBeLoadedObject()
+			@options.onAfterLoad() if typeof @options.onAfterLoad is 'function'
+
+		@$img.one 'bttrLoad', () =>
+			if !@loaded
+				@options.onBeforeLoad() if typeof @options.onBeforeLoad is 'function'
+				setTimeout ()=>
+					toBeLoaded = @_getToBeLoadedObject()
+					@$img.attr 'src', toBeLoaded.src
+				, 1000
+
+		$(window).bind  @options.event, () =>
 			@update()
 
 		$(window).bind "resize", () =>
+			console.log 'resize event'
 			@update()
 
-	_getSrc: () ->
+	_getToBeLoadedObject: () ->
 		ww = window.innerWidth
+		loadedObject = 
+			src : '',
+			level : ''
 		if (ww * @dpr) < @options.xs.width
-			src = @options.xs.src
+			loadedObject.level = 'xs'
+			loadedObject.src = if (@dpr > 1) then @options.xs.srcRetina else @options.xs.src
 		else if (ww * @dpr) < @options.sm.width
-			src = @options.sm.src
+			loadedObject.level = 'sm'
+			loadedObject.src = if (@dpr > 1) then @options.sm.srcRetina else @options.sm.src
 		else if (ww * @dpr) < @options.md.width
-			src = @options.md.src
+			loadedObject.level = 'md'
+			loadedObject.src = if (@dpr > 1) then @options.md.srcRetina else @options.md.src
 		else
-			src = @options.lg.src
-		src
+			loadedObject.level = 'lg'
+			loadedObject.src =if (@dpr > 1) then @options.lg.srcRetina else @options.lg.src
+		loadedObject
+
+	_isVisible: () ->
+		if @$img.is ':hidden'
+			return false
+
+		wt = @container.scrollTop()
+		wb = wt + @container.height()
+		et = @$img.offset().top
+		eb = et + @$img.height()
+		return eb >= wt - @options.threshold && et <= wb + @options.threshold;
 
 	###
 	public Functions
@@ -71,32 +120,67 @@ class bttrLazyLoading
 
 	update : () ->
 		if !@loaded
-			console.log @_getSrc(), 'loading'
-			@$img.attr 'src', @_getSrc()
-			@loaded = true
+			if @_isVisible()
+				@$img.trigger 'bttrLoad'
 		else
 			console.log 'updating'
+			
 
-	setOriginal : (original = {}) ->
-		$.extend(@options.original, original);
+	setThreshold : (threshold) ->
+		console.log 'threshold'
+		@options.threshold = threshold
 
 	setXs : (xs = {}) ->
 		$.extend(@options.xs, xs);
+		
+	setXsSrc : (xsSrc) ->
+		console.log 'setXsSrc'
+		@options.xs.src = xsSrc
+		
+	setXsSrcRetina : (xsSrc) ->
+		console.log 'setXsSrcRetina'
+		@options.xs.srcRetina = xsSrc
 
 	setSm : (sm = {}) ->
 		$.extend(@options.sm, sm);
+		
+	setSmSrc : (smSrc) ->
+		console.log 'setSmSrc'
+		@options.sm.src = smSrc
+		
+	setSmSrcRetina : (smSrc) ->
+		console.log 'setSmSrcRetina'
+		@options.sm.srcRetina = smSrc
 
 	setMd : (md = {}) ->
 		$.extend(@options.md, md);
+		
+	setMdSrc : (mdSrc) ->
+		console.log 'setMdSrc'
+		@options.md.src = mdSrc
+		
+	setMdSrcRetina : (mdSrc) ->
+		console.log 'setMdSrcRetina'
+		@options.md.srcRetina = mdSrc
 
 	setLg : (lg = {}) ->
 		$.extend(@options.lg, lg);
+		
+	setLgSrc : (lgSrc) ->
+		console.log 'setLgSrc'
+		@options.lg.src = lgSrc
+		
+	setLgSrcRetina : (lgSrc) ->
+		console.log 'setLgSrcRetina'
+		@options.lg.srcRetina = lgSrc
 
 jQuery.fn.extend
 	bttrlazyloading: (options) ->
 		this.each () ->
 			$this = $(this)
-			instance = new bttrLazyLoading(this, options)
-			$this.data 'bttrlazyloading', instance
-			console.log 'here'
+			# Already instanciated?
+			if !$this.hasClass 'bttrlazyloading-done'
+				instance = new bttrLazyLoading this, options
+				$this.addClass 'bttrlazyloading-done'
+				$this.data 'bttrlazyloading', instance
 		return this
--- bttrlazyloading.js
+++ bttrlazyloading.js
@@ -3,65 +3,62 @@
 
   bttrLazyLoading = (function() {
     function bttrLazyLoading(img, options) {
+      var _this = this;
       this.img = img;
       if (options == null) {
         options = {};
       }
       this.$img = $(this.img);
       this.loaded = false;
-      this.dpr = window.devicePixelRatio || 1;
+      this.levels = ['xs', 'sm', 'md', 'lg'];
       this.options = $.extend({
-        original: {
-          width: void 0,
-          height: void 0,
-          src: ''
-        },
         xs: {
           width: 768,
-          src: ''
+          src: '',
+          srcRetina: ''
         },
         sm: {
           width: 768,
-          src: ''
+          src: '',
+          srcRetina: ''
         },
         md: {
           width: 992,
-          src: ''
+          src: '',
+          srcRetina: ''
         },
         lg: {
           width: 1200,
-          src: ''
+          src: '',
+          srcRetina: ''
         },
-        effect: '',
+        effect: {
+          type: 'fadeIn',
+          duration: 200
+        },
         event: 'scroll',
-        container: document.body
+        container: window,
+        onBeforeLoad: function() {},
+        onAfterLoad: function() {},
+        threshold: 0,
+        placeholder: 'data:image/gif;base64,R0lGODlhEAALAPQAAP/391tbW+bf3+Da2vHq6l5dXVtbW3h2dq6qqpiVldLMzHBvb4qHh7Ovr5uYmNTOznNxcV1cXI2Kiu7n5+Xf3/fw8H58fOjh4fbv78/JycG8vNzW1vPs7AAAAAAAAAAAACH/C05FVFNDQVBFMi4wAwEAAAAh/hpDcmVhdGVkIHdpdGggYWpheGxvYWQuaW5mbwAh+QQJCwAAACwAAAAAEAALAAAFLSAgjmRpnqSgCuLKAq5AEIM4zDVw03ve27ifDgfkEYe04kDIDC5zrtYKRa2WQgAh+QQJCwAAACwAAAAAEAALAAAFJGBhGAVgnqhpHIeRvsDawqns0qeN5+y967tYLyicBYE7EYkYAgAh+QQJCwAAACwAAAAAEAALAAAFNiAgjothLOOIJAkiGgxjpGKiKMkbz7SN6zIawJcDwIK9W/HISxGBzdHTuBNOmcJVCyoUlk7CEAAh+QQJCwAAACwAAAAAEAALAAAFNSAgjqQIRRFUAo3jNGIkSdHqPI8Tz3V55zuaDacDyIQ+YrBH+hWPzJFzOQQaeavWi7oqnVIhACH5BAkLAAAALAAAAAAQAAsAAAUyICCOZGme1rJY5kRRk7hI0mJSVUXJtF3iOl7tltsBZsNfUegjAY3I5sgFY55KqdX1GgIAIfkECQsAAAAsAAAAABAACwAABTcgII5kaZ4kcV2EqLJipmnZhWGXaOOitm2aXQ4g7P2Ct2ER4AMul00kj5g0Al8tADY2y6C+4FIIACH5BAkLAAAALAAAAAAQAAsAAAUvICCOZGme5ERRk6iy7qpyHCVStA3gNa/7txxwlwv2isSacYUc+l4tADQGQ1mvpBAAIfkECQsAAAAsAAAAABAACwAABS8gII5kaZ7kRFGTqLLuqnIcJVK0DeA1r/u3HHCXC/aKxJpxhRz6Xi0ANAZDWa+kEAA7AAAAAAAAAAAA'
       }, options);
-      console.log(this.options);
-      if (this.$img.data('bttrlazyloading-original')) {
-        this.setOriginal({
-          src: this.$img.data('bttrlazyloading-original')
-        });
-      }
-      if (this.$img.data('bttrlazyloading-xs')) {
-        this.setXs({
-          src: this.$img.data('bttrlazyloading-xs')
-        });
-      }
-      if (this.$img.data('bttrlazyloading-sm')) {
-        this.setSm({
-          src: this.$img.data('bttrlazyloading-sm')
-        });
-      }
-      if (this.$img.data('bttrlazyloading-md')) {
-        this.setMd({
-          src: this.$img.data('bttrlazyloading-md')
-        });
-      }
-      if (this.$img.data('bttrlazyloading-lg')) {
-        this.setLg({
-          src: this.$img.data('bttrlazyloading-lg')
-        });
-      }
+      this.container = $(this.options.container);
+      this.dpr = this.container.devicePixelRatio || 1;
+      $.each(this.$img.data(), function(i, v) {
+        var method;
+        if (v) {
+          method = 'set' + i.replace('bttrlazyloading', '');
+          if (typeof _this[method] !== 'undefined') {
+            return _this[method](v);
+          }
+        }
+      });
+      this.$img.css({
+        'background-image': "url('" + this.options.placeholder + "')",
+        'background-repeat': 'no-repeat',
+        'background-position': 'center'
+      });
       this._setupEvents();
       this.update();
     }
@@ -76,27 +73,68 @@
       this.$img.bind(this.options.event, function() {
         return this.update();
       });
-      this.$img.one("appear", function() {
-        return this.update();
+      this.$img.bind('load', function() {
+        _this.$img.hide();
+        _this.$img[_this.options.effect.type](_this.options.effect.duration);
+        _this.loaded = _this._getToBeLoadedObject();
+        if (typeof _this.options.onAfterLoad === 'function') {
+          return _this.options.onAfterLoad();
+        }
+      });
+      this.$img.one('bttrLoad', function() {
+        if (!_this.loaded) {
+          if (typeof _this.options.onBeforeLoad === 'function') {
+            _this.options.onBeforeLoad();
+          }
+          return setTimeout(function() {
+            var toBeLoaded;
+            toBeLoaded = _this._getToBeLoadedObject();
+            return _this.$img.attr('src', toBeLoaded.src);
+          }, 1000);
+        }
+      });
+      $(window).bind(this.options.event, function() {
+        return _this.update();
       });
       return $(window).bind("resize", function() {
+        console.log('resize event');
         return _this.update();
       });
     };
 
-    bttrLazyLoading.prototype._getSrc = function() {
-      var src, ww;
+    bttrLazyLoading.prototype._getToBeLoadedObject = function() {
+      var loadedObject, ww;
       ww = window.innerWidth;
+      loadedObject = {
+        src: '',
+        level: ''
+      };
       if ((ww * this.dpr) < this.options.xs.width) {
-        src = this.options.xs.src;
+        loadedObject.level = 'xs';
+        loadedObject.src = this.dpr > 1 ? this.options.xs.srcRetina : this.options.xs.src;
       } else if ((ww * this.dpr) < this.options.sm.width) {
-        src = this.options.sm.src;
+        loadedObject.level = 'sm';
+        loadedObject.src = this.dpr > 1 ? this.options.sm.srcRetina : this.options.sm.src;
       } else if ((ww * this.dpr) < this.options.md.width) {
-        src = this.options.md.src;
+        loadedObject.level = 'md';
+        loadedObject.src = this.dpr > 1 ? this.options.md.srcRetina : this.options.md.src;
       } else {
-        src = this.options.lg.src;
+        loadedObject.level = 'lg';
+        loadedObject.src = this.dpr > 1 ? this.options.lg.srcRetina : this.options.lg.src;
       }
-      return src;
+      return loadedObject;
+    };
+
+    bttrLazyLoading.prototype._isVisible = function() {
+      var eb, et, wb, wt;
+      if (this.$img.is(':hidden')) {
+        return false;
+      }
+      wt = this.container.scrollTop();
+      wb = wt + this.container.height();
+      et = this.$img.offset().top;
+      eb = et + this.$img.height();
+      return eb >= wt - this.options.threshold && et <= wb + this.options.threshold;
     };
 
     /*
@@ -106,19 +144,17 @@
 
     bttrLazyLoading.prototype.update = function() {
       if (!this.loaded) {
-        console.log(this._getSrc(), 'loading');
-        this.$img.attr('src', this._getSrc());
-        return this.loaded = true;
+        if (this._isVisible()) {
+          return this.$img.trigger('bttrLoad');
+        }
       } else {
         return console.log('updating');
       }
     };
 
-    bttrLazyLoading.prototype.setOriginal = function(original) {
-      if (original == null) {
-        original = {};
-      }
-      return $.extend(this.options.original, original);
+    bttrLazyLoading.prototype.setThreshold = function(threshold) {
+      console.log('threshold');
+      return this.options.threshold = threshold;
     };
 
     bttrLazyLoading.prototype.setXs = function(xs) {
@@ -128,11 +164,31 @@
       return $.extend(this.options.xs, xs);
     };
 
+    bttrLazyLoading.prototype.setXsSrc = function(xsSrc) {
+      console.log('setXsSrc');
+      return this.options.xs.src = xsSrc;
+    };
+
+    bttrLazyLoading.prototype.setXsSrcRetina = function(xsSrc) {
+      console.log('setXsSrcRetina');
+      return this.options.xs.srcRetina = xsSrc;
+    };
+
     bttrLazyLoading.prototype.setSm = function(sm) {
       if (sm == null) {
         sm = {};
       }
       return $.extend(this.options.sm, sm);
+    };
+
+    bttrLazyLoading.prototype.setSmSrc = function(smSrc) {
+      console.log('setSmSrc');
+      return this.options.sm.src = smSrc;
+    };
+
+    bttrLazyLoading.prototype.setSmSrcRetina = function(smSrc) {
+      console.log('setSmSrcRetina');
+      return this.options.sm.srcRetina = smSrc;
     };
 
     bttrLazyLoading.prototype.setMd = function(md) {
@@ -142,11 +198,31 @@
       return $.extend(this.options.md, md);
     };
 
+    bttrLazyLoading.prototype.setMdSrc = function(mdSrc) {
+      console.log('setMdSrc');
+      return this.options.md.src = mdSrc;
+    };
+
+    bttrLazyLoading.prototype.setMdSrcRetina = function(mdSrc) {
+      console.log('setMdSrcRetina');
+      return this.options.md.srcRetina = mdSrc;
+    };
+
     bttrLazyLoading.prototype.setLg = function(lg) {
       if (lg == null) {
         lg = {};
       }
       return $.extend(this.options.lg, lg);
+    };
+
+    bttrLazyLoading.prototype.setLgSrc = function(lgSrc) {
+      console.log('setLgSrc');
+      return this.options.lg.src = lgSrc;
+    };
+
+    bttrLazyLoading.prototype.setLgSrcRetina = function(lgSrc) {
+      console.log('setLgSrcRetina');
+      return this.options.lg.srcRetina = lgSrc;
     };
 
     return bttrLazyLoading;
@@ -158,9 +234,11 @@
       this.each(function() {
         var $this, instance;
         $this = $(this);
-        instance = new bttrLazyLoading(this, options);
-        $this.data('bttrlazyloading', instance);
-        return console.log('here');
+        if (!$this.hasClass('bttrlazyloading-done')) {
+          instance = new bttrLazyLoading(this, options);
+          $this.addClass('bttrlazyloading-done');
+          return $this.data('bttrlazyloading', instance);
+        }
       });
       return this;
     }
