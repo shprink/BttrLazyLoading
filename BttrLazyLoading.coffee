846a87968c26deece4ce39137bdda5e617301b7a
shprink <julien.renaux@gmail.com>
2013-11-20 02:30:33 +0100

    adding width and height options per img size

--- BttrLazyLoading.coffee
+++ BttrLazyLoading.coffee
@@ -2,41 +2,14 @@ $ = jQuery
 
 class BttrLazyLoading
 
-	@DEFAULT: {
-			xs :
-				src : null
-			sm :
-				src : null
-			md :
-				src : null
-			lg :
-				src : null
-			ranges : undefined
-			retinaEnabled : undefined
-			transitionDuration : 200
-			event : 'scroll',
-			container : window,
-			onBeforeLoad : ($img, bttrLazyLoading) ->
-				return
-			onAfterLoad : ($img, bttrLazyLoading) ->
-				return
-			onError : ($img, bttrLazyLoading) ->
-				#$img.remove()
-				return
-			threshold : 0,
-			placeholder : 'data:image/gif;base64,R0lGODlhEAALAPQAAP/391tbW+bf3+Da2vHq6l5dXVtbW3h2dq6qqpiVldLMzHBvb4qHh7Ovr5uYmNTOznNxcV1cXI2Kiu7n5+Xf3/fw8H58fOjh4fbv78/JycG8vNzW1vPs7AAAAAAAAAAAACH/C05FVFNDQVBFMi4wAwEAAAAh/hpDcmVhdGVkIHdpdGggYWpheGxvYWQuaW5mbwAh+QQJCwAAACwAAAAAEAALAAAFLSAgjmRpnqSgCuLKAq5AEIM4zDVw03ve27ifDgfkEYe04kDIDC5zrtYKRa2WQgAh+QQJCwAAACwAAAAAEAALAAAFJGBhGAVgnqhpHIeRvsDawqns0qeN5+y967tYLyicBYE7EYkYAgAh+QQJCwAAACwAAAAAEAALAAAFNiAgjothLOOIJAkiGgxjpGKiKMkbz7SN6zIawJcDwIK9W/HISxGBzdHTuBNOmcJVCyoUlk7CEAAh+QQJCwAAACwAAAAAEAALAAAFNSAgjqQIRRFUAo3jNGIkSdHqPI8Tz3V55zuaDacDyIQ+YrBH+hWPzJFzOQQaeavWi7oqnVIhACH5BAkLAAAALAAAAAAQAAsAAAUyICCOZGme1rJY5kRRk7hI0mJSVUXJtF3iOl7tltsBZsNfUegjAY3I5sgFY55KqdX1GgIAIfkECQsAAAAsAAAAABAACwAABTcgII5kaZ4kcV2EqLJipmnZhWGXaOOitm2aXQ4g7P2Ct2ER4AMul00kj5g0Al8tADY2y6C+4FIIACH5BAkLAAAALAAAAAAQAAsAAAUvICCOZGme5ERRk6iy7qpyHCVStA3gNa/7txxwlwv2isSacYUc+l4tADQGQ1mvpBAAIfkECQsAAAAsAAAAABAACwAABS8gII5kaZ7kRFGTqLLuqnIcJVK0DeA1r/u3HHCXC/aKxJpxhRz6Xi0ANAZDWa+kEAA7AAAAAAAAAAAA'
-		}
+	#@DEFAULT: 
 
 	constructor: (@img, options = {}) ->
 		@$img = $(@img)
 		@loaded = false
 		@rangesOrder = ['xs','sm', 'md', 'lg']
 		
-		defaultOptions = this.constructor.DEFAULT
-
-		# Mergin Global options
-		defaultOptions.ranges = $.bttrlazyloading.constructor.ranges
-		defaultOptions.retinaEnabled = $.bttrlazyloading.constructor.retinaEnabled
+		defaultOptions = $.extend true, {}, $.bttrlazyloading.constructor.options
 
 		@options = $.extend defaultOptions, options
 		console.log @options
@@ -50,13 +23,18 @@ class BttrLazyLoading
 				method = 'set' + i.replace 'bttrlazyloading', ''
 				this[method](v) if typeof this[method] isnt 'undefined'
 
-		console.log @$img.width()
-		console.log @$img.height()
+		imgObject = @_getScreenSrc()
+		console.log imgObject, 'imgObject start'
+#		@$img.attr 'width', @$img[0].naturalWidth
+#		@$img.attr 'height', @$img[0].naturalHeight
+			
 		@$img.css
-			'background-image'		: "url('" + @options.placeholder + "')",
-			'background-repeat'		: 'no-repeat',
+			'background-image'		: "url('" + @options.placeholder + "')"
+			'background-repeat'		: 'no-repeat'
 			'background-position'	: 'center'
-		
+			'width'					: imgObject.width
+			'height'				: imgObject.height
+
 		@_setupEvents()
 
 		@update()
@@ -70,26 +48,24 @@ class BttrLazyLoading
 
 		@$img.bind 'load', () =>
 			@$img.addClass 'bttrlazyloading-loaded'
-			@$img.css 'opacity', 0
-			@$img.animate
-				opacity : 1
-			, @options.transitionDuration
-			# Does not work on IE8
-			@$img.attr 'width', @$img[0].naturalWidth
-			@$img.attr 'height', @$img[0].naturalHeight
+			@$img.addClass 'animated ' + @options.transition if @options.transition
 			@loaded = @$img.attr 'src'
 			@options.onAfterLoad(@$img, this) if typeof @options.onAfterLoad is 'function'
 
 		@$img.one 'bttrLoad', () =>
 			if !@loaded
 				@options.onBeforeLoad(@$img, this) if typeof @options.onBeforeLoad is 'function'
-				src = @_getScreenSrc()
-				console.log src, 'bttrLoad src'
-
-				if (@dpr > 1 && @options.retinaEnabled)
-					@$img.attr 'src', @_getRetinaSrc src
-				else
-					@$img.attr 'src', src
+				imgObject = @_getScreenSrc()
+				console.log imgObject, 'bttrLoad imgObject'
+				setTimeout () =>
+					if (@dpr > 1 && @options.retinaEnabled)
+						@$img.attr 'src', @_getRetinaSrc imgObject.src
+					else
+						@$img.attr 'src', imgObject.src
+					@$img.css
+						'width'		: 'auto'
+						'height'	: 'auto'
+				, 3000
 
 		$(window).bind  @options.event, () =>
 			@update()
@@ -100,45 +76,45 @@ class BttrLazyLoading
 	_getScreenSrc: () ->
 		ww = window.innerWidth
 		if (ww * @dpr) <= @options.ranges.xs
-			@_getLargestExistingSrc 'xs'
+			@_getLargestExistingRangeObject 'xs'
 		else if @options.ranges.sm <= (ww * @dpr) < @options.ranges.md
-			@_getLargestExistingSrc 'sm'
+			@_getLargestExistingRangeObject 'sm'
 		else if @options.ranges.md <= (ww * @dpr) < @options.ranges.lg
-			@_getLargestExistingSrc 'md'
+			@_getLargestExistingRangeObject 'md'
 		else if @options.ranges.lg <= (ww * @dpr)
-			@_getLargestExistingSrc 'lg'
+			@_getLargestExistingRangeObject 'lg'
 			
 	_getRetinaSrc: (src)->
 		src.replace(/\.\w+$/, (match)->
 				return "@2x" + match
 			)
 
-	_getSrc: (range)->
-		if typeof @options[range].src isnt 'undefined' and @options[range].src isnt null
-			return @options[range].src
-		return ''
+	_getRangeObject: (range)->
+		if typeof @options.img[range].src isnt 'undefined' and @options.img[range].src isnt null
+			return @options.img[range]
+		return false
 
-	_getLargestExistingSrc: (range)->
+	_getLargestExistingRangeObject: (range)->
 		index = @rangesOrder.indexOf(range)
 
 		# Check if the right img exist
-		src = @_getSrc(range)
-		return src if src isnt ''
+		src = @_getRangeObject(range)
+		return src if typeof src == 'object'
 
 		# If not we check if a bigger img exist
 		max = @rangesOrder.length - 1
 		for i in [index .. max]
 			range = @rangesOrder[i]
-			srcTemp = @_getSrc(range)
+			srcTemp = @_getRangeObject(range)
 			src =  srcTemp if srcTemp
-		return src if src isnt ''
+		return src if typeof src == 'object'
 
 		# If not we start back from the smallest img
 		for i in [0 .. index]
 			range = @rangesOrder[i]
-			srcTemp = @_getSrc(range)
+			srcTemp = @_getRangeObject(range)
 			src =  srcTemp if srcTemp
-		return src if src isnt ''
+		return src if typeof src == 'object'
 
 		@options.onError(@$img, this) if typeof @options.onError is 'function'
 		return ''
@@ -163,15 +139,15 @@ class BttrLazyLoading
 				@$img.trigger 'bttrLoad'
 		else
 			if @_isVisible()
-				src = @_getScreenSrc()
-				console.log src, 'update'
-				if src and @loaded isnt src
+				imgObject = @_getScreenSrc()
+				console.log imgObject, 'update imgObject'
+				if imgObject.src and @loaded isnt imgObject.src
 					@$img.removeClass 'bttrlazyloading-loaded'
-					@loaded = src
+					@loaded = imgObject.src
 					if (@dpr > 1 && @options.retinaEnabled)
-						@$img.attr 'src', @_getRetinaSrc src
+						@$img.attr 'src', @_getRetinaSrc imgObjectsrc
 					else
-						@$img.attr 'src', src
+						@$img.attr 'src', imgObject.src
 			
 
 	setThreshold : (threshold) ->
@@ -186,36 +162,56 @@ class BttrLazyLoading
 	setPlaceholder : (placeholder = '') ->
 		@options.placeholder = placeholder
 
-	setTransitionDuration : (transitionDuration) ->
-		@options.transitionDuration = transitionDuration
+	setTransition : (transition) ->
+		@options.transition = transition
 
 	setXs : (xs = {}) ->
-		$.extend(@options.xs, xs);
+		$.extend(@options.img.xs, xs);
 		
 	setXsSrc : (xsSrc) ->
-		#console.log 'setXsSrc'
-		@options.xs.src = xsSrc
+		@options.img.xs.src = xsSrc
+		
+	setXsWidth : (width) ->
+		@options.img.xs.width = width
+		
+	setXsHeight : (height) ->
+		@options.img.xs.height = height
 
 	setSm : (sm = {}) ->
-		$.extend(@options.sm, sm);
+		$.extend(@options.img.sm, sm);
 		
 	setSmSrc : (smSrc) ->
-		#console.log 'setSmSrc'
-		@options.sm.src = smSrc
+		@options.img.sm.src = smSrc
+		
+	setSmWidth : (width) ->
+		@options.img.sm.width = width
+		
+	setSmHeight : (height) ->
+		@options.img.sm.height = height
 
 	setMd : (md = {}) ->
-		$.extend(@options.md, md);
+		$.extend(@options.img.md, md);
 		
 	setMdSrc : (mdSrc) ->
-		#console.log 'setMdSrc'
-		@options.md.src = mdSrc
+		@options.img.md.src = mdSrc
+		
+	setMdWidth : (width) ->
+		@options.img.md.width = width
+		
+	setMdHeight : (height) ->
+		@options.img.md.height = height
 
 	setLg : (lg = {}) ->
-		$.extend(@options.lg, lg);
+		$.extend(@options.img.lg, lg);
 		
 	setLgSrc : (lgSrc) ->
-		#console.log 'setLgSrc'
-		@options.lg.src = lgSrc
+		@options.img.lg.src = lgSrc
+		
+	setLgWidth : (width) ->
+		@options.img.lg.width = width
+		
+	setLgHeight : (height) ->
+		@options.img.lg.height = height
 
 $.fn.extend
 	bttrlazyloading: (options) ->
@@ -231,22 +227,46 @@ $.fn.bttrlazyloading.Constructor = BttrLazyLoading
 
 class BttrLazyLoadingGlobal
 
-	version : '1.0.0'
-	@ranges : 
-		'xs' : 767
-		'sm' : 768
-		'md' : 992
-		'lg' : 1200
-	
-	@retinaEnabled : false
+	version : '1.0.0'		
+	@options =
+		img :
+			xs :
+				src : null
+				width : 100
+				height : 100
+			sm :
+				src : null
+				width : 100
+				height : 100
+			md :
+				src : null
+				width : 100
+				height : 100
+			lg :
+				src : null
+				width : 100
+				height : 100
+		ranges : 
+			'xs' : 767
+			'sm' : 768
+			'md' : 992
+			'lg' : 1200
+		retinaEnabled : false
+		transition: 'bounceIn'
+		event : 'scroll',
+		container : window,
+		onBeforeLoad : ($img, bttrLazyLoading) ->
+			return
+		onAfterLoad : ($img, bttrLazyLoading) ->
+			return
+		onError : ($img, bttrLazyLoading) ->
+			#$img.remove()
+			return
+		threshold : 0,
+		placeholder : 'data:image/gif;base64,R0lGODlhEAALAPQAAP/391tbW+bf3+Da2vHq6l5dXVtbW3h2dq6qqpiVldLMzHBvb4qHh7Ovr5uYmNTOznNxcV1cXI2Kiu7n5+Xf3/fw8H58fOjh4fbv78/JycG8vNzW1vPs7AAAAAAAAAAAACH/C05FVFNDQVBFMi4wAwEAAAAh/hpDcmVhdGVkIHdpdGggYWpheGxvYWQuaW5mbwAh+QQJCwAAACwAAAAAEAALAAAFLSAgjmRpnqSgCuLKAq5AEIM4zDVw03ve27ifDgfkEYe04kDIDC5zrtYKRa2WQgAh+QQJCwAAACwAAAAAEAALAAAFJGBhGAVgnqhpHIeRvsDawqns0qeN5+y967tYLyicBYE7EYkYAgAh+QQJCwAAACwAAAAAEAALAAAFNiAgjothLOOIJAkiGgxjpGKiKMkbz7SN6zIawJcDwIK9W/HISxGBzdHTuBNOmcJVCyoUlk7CEAAh+QQJCwAAACwAAAAAEAALAAAFNSAgjqQIRRFUAo3jNGIkSdHqPI8Tz3V55zuaDacDyIQ+YrBH+hWPzJFzOQQaeavWi7oqnVIhACH5BAkLAAAALAAAAAAQAAsAAAUyICCOZGme1rJY5kRRk7hI0mJSVUXJtF3iOl7tltsBZsNfUegjAY3I5sgFY55KqdX1GgIAIfkECQsAAAAsAAAAABAACwAABTcgII5kaZ4kcV2EqLJipmnZhWGXaOOitm2aXQ4g7P2Ct2ER4AMul00kj5g0Al8tADY2y6C+4FIIACH5BAkLAAAALAAAAAAQAAsAAAUvICCOZGme5ERRk6iy7qpyHCVStA3gNa/7txxwlwv2isSacYUc+l4tADQGQ1mvpBAAIfkECQsAAAAsAAAAABAACwAABS8gII5kaZ7kRFGTqLLuqnIcJVK0DeA1r/u3HHCXC/aKxJpxhRz6Xi0ANAZDWa+kEAA7AAAAAAAAAAAA'
 
-	setDefaultRanges : (object = {}) ->
-		$.extend this.constructor.ranges, object
-		this
-
-	setRetina : (boolean) ->
-		if typeof boolean == 'boolean'
-			this.constructor.retinaEnabled = boolean
+	setOptions : (object = {}) ->
+		$.extend true, this.constructor.options, object
 		this
 
 $.bttrlazyloading = new BttrLazyLoadingGlobal()
--- bttrlazyloading.js
+++ bttrlazyloading.js
@@ -4,33 +4,8 @@
   $ = jQuery;
 
   BttrLazyLoading = (function() {
-    BttrLazyLoading.DEFAULT = {
-      xs: {
-        src: null
-      },
-      sm: {
-        src: null
-      },
-      md: {
-        src: null
-      },
-      lg: {
-        src: null
-      },
-      ranges: void 0,
-      retinaEnabled: void 0,
-      transitionDuration: 200,
-      event: 'scroll',
-      container: window,
-      onBeforeLoad: function($img, bttrLazyLoading) {},
-      onAfterLoad: function($img, bttrLazyLoading) {},
-      onError: function($img, bttrLazyLoading) {},
-      threshold: 0,
-      placeholder: 'data:image/gif;base64,R0lGODlhEAALAPQAAP/391tbW+bf3+Da2vHq6l5dXVtbW3h2dq6qqpiVldLMzHBvb4qHh7Ovr5uYmNTOznNxcV1cXI2Kiu7n5+Xf3/fw8H58fOjh4fbv78/JycG8vNzW1vPs7AAAAAAAAAAAACH/C05FVFNDQVBFMi4wAwEAAAAh/hpDcmVhdGVkIHdpdGggYWpheGxvYWQuaW5mbwAh+QQJCwAAACwAAAAAEAALAAAFLSAgjmRpnqSgCuLKAq5AEIM4zDVw03ve27ifDgfkEYe04kDIDC5zrtYKRa2WQgAh+QQJCwAAACwAAAAAEAALAAAFJGBhGAVgnqhpHIeRvsDawqns0qeN5+y967tYLyicBYE7EYkYAgAh+QQJCwAAACwAAAAAEAALAAAFNiAgjothLOOIJAkiGgxjpGKiKMkbz7SN6zIawJcDwIK9W/HISxGBzdHTuBNOmcJVCyoUlk7CEAAh+QQJCwAAACwAAAAAEAALAAAFNSAgjqQIRRFUAo3jNGIkSdHqPI8Tz3V55zuaDacDyIQ+YrBH+hWPzJFzOQQaeavWi7oqnVIhACH5BAkLAAAALAAAAAAQAAsAAAUyICCOZGme1rJY5kRRk7hI0mJSVUXJtF3iOl7tltsBZsNfUegjAY3I5sgFY55KqdX1GgIAIfkECQsAAAAsAAAAABAACwAABTcgII5kaZ4kcV2EqLJipmnZhWGXaOOitm2aXQ4g7P2Ct2ER4AMul00kj5g0Al8tADY2y6C+4FIIACH5BAkLAAAALAAAAAAQAAsAAAUvICCOZGme5ERRk6iy7qpyHCVStA3gNa/7txxwlwv2isSacYUc+l4tADQGQ1mvpBAAIfkECQsAAAAsAAAAABAACwAABS8gII5kaZ7kRFGTqLLuqnIcJVK0DeA1r/u3HHCXC/aKxJpxhRz6Xi0ANAZDWa+kEAA7AAAAAAAAAAAA'
-    };
-
     function BttrLazyLoading(img, options) {
-      var defaultOptions,
+      var defaultOptions, imgObject,
         _this = this;
       this.img = img;
       if (options == null) {
@@ -39,9 +14,7 @@
       this.$img = $(this.img);
       this.loaded = false;
       this.rangesOrder = ['xs', 'sm', 'md', 'lg'];
-      defaultOptions = this.constructor.DEFAULT;
-      defaultOptions.ranges = $.bttrlazyloading.constructor.ranges;
-      defaultOptions.retinaEnabled = $.bttrlazyloading.constructor.retinaEnabled;
+      defaultOptions = $.extend(true, {}, $.bttrlazyloading.constructor.options);
       this.options = $.extend(defaultOptions, options);
       console.log(this.options);
       this.container = $(this.options.container);
@@ -55,12 +28,14 @@
           }
         }
       });
-      console.log(this.$img.width());
-      console.log(this.$img.height());
+      imgObject = this._getScreenSrc();
+      console.log(imgObject, 'imgObject start');
       this.$img.css({
         'background-image': "url('" + this.options.placeholder + "')",
         'background-repeat': 'no-repeat',
-        'background-position': 'center'
+        'background-position': 'center',
+        'width': imgObject.width,
+        'height': imgObject.height
       });
       this._setupEvents();
       this.update();
@@ -78,30 +53,33 @@
       });
       this.$img.bind('load', function() {
         _this.$img.addClass('bttrlazyloading-loaded');
-        _this.$img.css('opacity', 0);
-        _this.$img.animate({
-          opacity: 1
-        }, _this.options.transitionDuration);
-        _this.$img.attr('width', _this.$img[0].naturalWidth);
-        _this.$img.attr('height', _this.$img[0].naturalHeight);
+        if (_this.options.transition) {
+          _this.$img.addClass('animated ' + _this.options.transition);
+        }
         _this.loaded = _this.$img.attr('src');
         if (typeof _this.options.onAfterLoad === 'function') {
           return _this.options.onAfterLoad(_this.$img, _this);
         }
       });
       this.$img.one('bttrLoad', function() {
-        var src;
+        var imgObject;
         if (!_this.loaded) {
           if (typeof _this.options.onBeforeLoad === 'function') {
             _this.options.onBeforeLoad(_this.$img, _this);
           }
-          src = _this._getScreenSrc();
-          console.log(src, 'bttrLoad src');
-          if (_this.dpr > 1 && _this.options.retinaEnabled) {
-            return _this.$img.attr('src', _this._getRetinaSrc(src));
-          } else {
-            return _this.$img.attr('src', src);
-          }
+          imgObject = _this._getScreenSrc();
+          console.log(imgObject, 'bttrLoad imgObject');
+          return setTimeout(function() {
+            if (_this.dpr > 1 && _this.options.retinaEnabled) {
+              _this.$img.attr('src', _this._getRetinaSrc(imgObject.src));
+            } else {
+              _this.$img.attr('src', imgObject.src);
+            }
+            return _this.$img.css({
+              'width': 'auto',
+              'height': 'auto'
+            });
+          }, 3000);
         }
       });
       $(window).bind(this.options.event, function() {
@@ -116,13 +94,13 @@
       var ww, _ref, _ref1;
       ww = window.innerWidth;
       if ((ww * this.dpr) <= this.options.ranges.xs) {
-        return this._getLargestExistingSrc('xs');
+        return this._getLargestExistingRangeObject('xs');
       } else if ((this.options.ranges.sm <= (_ref = ww * this.dpr) && _ref < this.options.ranges.md)) {
-        return this._getLargestExistingSrc('sm');
+        return this._getLargestExistingRangeObject('sm');
       } else if ((this.options.ranges.md <= (_ref1 = ww * this.dpr) && _ref1 < this.options.ranges.lg)) {
-        return this._getLargestExistingSrc('md');
+        return this._getLargestExistingRangeObject('md');
       } else if (this.options.ranges.lg <= (ww * this.dpr)) {
-        return this._getLargestExistingSrc('lg');
+        return this._getLargestExistingRangeObject('lg');
       }
     };
 
@@ -132,39 +110,39 @@
       });
     };
 
-    BttrLazyLoading.prototype._getSrc = function(range) {
-      if (typeof this.options[range].src !== 'undefined' && this.options[range].src !== null) {
-        return this.options[range].src;
+    BttrLazyLoading.prototype._getRangeObject = function(range) {
+      if (typeof this.options.img[range].src !== 'undefined' && this.options.img[range].src !== null) {
+        return this.options.img[range];
       }
-      return '';
+      return false;
     };
 
-    BttrLazyLoading.prototype._getLargestExistingSrc = function(range) {
+    BttrLazyLoading.prototype._getLargestExistingRangeObject = function(range) {
       var i, index, max, src, srcTemp, _i, _j;
       index = this.rangesOrder.indexOf(range);
-      src = this._getSrc(range);
-      if (src !== '') {
+      src = this._getRangeObject(range);
+      if (typeof src === 'object') {
         return src;
       }
       max = this.rangesOrder.length - 1;
       for (i = _i = index; index <= max ? _i <= max : _i >= max; i = index <= max ? ++_i : --_i) {
         range = this.rangesOrder[i];
-        srcTemp = this._getSrc(range);
+        srcTemp = this._getRangeObject(range);
         if (srcTemp) {
           src = srcTemp;
         }
       }
-      if (src !== '') {
+      if (typeof src === 'object') {
         return src;
       }
       for (i = _j = 0; 0 <= index ? _j <= index : _j >= index; i = 0 <= index ? ++_j : --_j) {
         range = this.rangesOrder[i];
-        srcTemp = this._getSrc(range);
+        srcTemp = this._getRangeObject(range);
         if (srcTemp) {
           src = srcTemp;
         }
       }
-      if (src !== '') {
+      if (typeof src === 'object') {
         return src;
       }
       if (typeof this.options.onError === 'function') {
@@ -191,22 +169,22 @@
 
 
     BttrLazyLoading.prototype.update = function() {
-      var src;
+      var imgObject;
       if (!this.loaded) {
         if (this._isVisible()) {
           return this.$img.trigger('bttrLoad');
         }
       } else {
         if (this._isVisible()) {
-          src = this._getScreenSrc();
-          console.log(src, 'update');
-          if (src && this.loaded !== src) {
+          imgObject = this._getScreenSrc();
+          console.log(imgObject, 'update imgObject');
+          if (imgObject.src && this.loaded !== imgObject.src) {
             this.$img.removeClass('bttrlazyloading-loaded');
-            this.loaded = src;
+            this.loaded = imgObject.src;
             if (this.dpr > 1 && this.options.retinaEnabled) {
-              return this.$img.attr('src', this._getRetinaSrc(src));
+              return this.$img.attr('src', this._getRetinaSrc(imgObjectsrc));
             } else {
-              return this.$img.attr('src', src);
+              return this.$img.attr('src', imgObject.src);
             }
           }
         }
@@ -235,52 +213,84 @@
       return this.options.placeholder = placeholder;
     };
 
-    BttrLazyLoading.prototype.setTransitionDuration = function(transitionDuration) {
-      return this.options.transitionDuration = transitionDuration;
+    BttrLazyLoading.prototype.setTransition = function(transition) {
+      return this.options.transition = transition;
     };
 
     BttrLazyLoading.prototype.setXs = function(xs) {
       if (xs == null) {
         xs = {};
       }
-      return $.extend(this.options.xs, xs);
+      return $.extend(this.options.img.xs, xs);
     };
 
     BttrLazyLoading.prototype.setXsSrc = function(xsSrc) {
-      return this.options.xs.src = xsSrc;
+      return this.options.img.xs.src = xsSrc;
+    };
+
+    BttrLazyLoading.prototype.setXsWidth = function(width) {
+      return this.options.img.xs.width = width;
+    };
+
+    BttrLazyLoading.prototype.setXsHeight = function(height) {
+      return this.options.img.xs.height = height;
     };
 
     BttrLazyLoading.prototype.setSm = function(sm) {
       if (sm == null) {
         sm = {};
       }
-      return $.extend(this.options.sm, sm);
+      return $.extend(this.options.img.sm, sm);
     };
 
     BttrLazyLoading.prototype.setSmSrc = function(smSrc) {
-      return this.options.sm.src = smSrc;
+      return this.options.img.sm.src = smSrc;
+    };
+
+    BttrLazyLoading.prototype.setSmWidth = function(width) {
+      return this.options.img.sm.width = width;
+    };
+
+    BttrLazyLoading.prototype.setSmHeight = function(height) {
+      return this.options.img.sm.height = height;
     };
 
     BttrLazyLoading.prototype.setMd = function(md) {
       if (md == null) {
         md = {};
       }
-      return $.extend(this.options.md, md);
+      return $.extend(this.options.img.md, md);
     };
 
     BttrLazyLoading.prototype.setMdSrc = function(mdSrc) {
-      return this.options.md.src = mdSrc;
+      return this.options.img.md.src = mdSrc;
+    };
+
+    BttrLazyLoading.prototype.setMdWidth = function(width) {
+      return this.options.img.md.width = width;
+    };
+
+    BttrLazyLoading.prototype.setMdHeight = function(height) {
+      return this.options.img.md.height = height;
     };
 
     BttrLazyLoading.prototype.setLg = function(lg) {
       if (lg == null) {
         lg = {};
       }
-      return $.extend(this.options.lg, lg);
+      return $.extend(this.options.img.lg, lg);
     };
 
     BttrLazyLoading.prototype.setLgSrc = function(lgSrc) {
-      return this.options.lg.src = lgSrc;
+      return this.options.img.lg.src = lgSrc;
+    };
+
+    BttrLazyLoading.prototype.setLgWidth = function(width) {
+      return this.options.img.lg.width = width;
+    };
+
+    BttrLazyLoading.prototype.setLgHeight = function(height) {
+      return this.options.img.lg.height = height;
     };
 
     return BttrLazyLoading;
@@ -308,27 +318,51 @@
 
     BttrLazyLoadingGlobal.prototype.version = '1.0.0';
 
-    BttrLazyLoadingGlobal.ranges = {
-      'xs': 767,
-      'sm': 768,
-      'md': 992,
-      'lg': 1200
+    BttrLazyLoadingGlobal.options = {
+      img: {
+        xs: {
+          src: null,
+          width: 100,
+          height: 100
+        },
+        sm: {
+          src: null,
+          width: 100,
+          height: 100
+        },
+        md: {
+          src: null,
+          width: 100,
+          height: 100
+        },
+        lg: {
+          src: null,
+          width: 100,
+          height: 100
+        }
+      },
+      ranges: {
+        'xs': 767,
+        'sm': 768,
+        'md': 992,
+        'lg': 1200
+      },
+      retinaEnabled: false,
+      transition: 'bounceIn',
+      event: 'scroll',
+      container: window,
+      onBeforeLoad: function($img, bttrLazyLoading) {},
+      onAfterLoad: function($img, bttrLazyLoading) {},
+      onError: function($img, bttrLazyLoading) {},
+      threshold: 0,
+      placeholder: 'data:image/gif;base64,R0lGODlhEAALAPQAAP/391tbW+bf3+Da2vHq6l5dXVtbW3h2dq6qqpiVldLMzHBvb4qHh7Ovr5uYmNTOznNxcV1cXI2Kiu7n5+Xf3/fw8H58fOjh4fbv78/JycG8vNzW1vPs7AAAAAAAAAAAACH/C05FVFNDQVBFMi4wAwEAAAAh/hpDcmVhdGVkIHdpdGggYWpheGxvYWQuaW5mbwAh+QQJCwAAACwAAAAAEAALAAAFLSAgjmRpnqSgCuLKAq5AEIM4zDVw03ve27ifDgfkEYe04kDIDC5zrtYKRa2WQgAh+QQJCwAAACwAAAAAEAALAAAFJGBhGAVgnqhpHIeRvsDawqns0qeN5+y967tYLyicBYE7EYkYAgAh+QQJCwAAACwAAAAAEAALAAAFNiAgjothLOOIJAkiGgxjpGKiKMkbz7SN6zIawJcDwIK9W/HISxGBzdHTuBNOmcJVCyoUlk7CEAAh+QQJCwAAACwAAAAAEAALAAAFNSAgjqQIRRFUAo3jNGIkSdHqPI8Tz3V55zuaDacDyIQ+YrBH+hWPzJFzOQQaeavWi7oqnVIhACH5BAkLAAAALAAAAAAQAAsAAAUyICCOZGme1rJY5kRRk7hI0mJSVUXJtF3iOl7tltsBZsNfUegjAY3I5sgFY55KqdX1GgIAIfkECQsAAAAsAAAAABAACwAABTcgII5kaZ4kcV2EqLJipmnZhWGXaOOitm2aXQ4g7P2Ct2ER4AMul00kj5g0Al8tADY2y6C+4FIIACH5BAkLAAAALAAAAAAQAAsAAAUvICCOZGme5ERRk6iy7qpyHCVStA3gNa/7txxwlwv2isSacYUc+l4tADQGQ1mvpBAAIfkECQsAAAAsAAAAABAACwAABS8gII5kaZ7kRFGTqLLuqnIcJVK0DeA1r/u3HHCXC/aKxJpxhRz6Xi0ANAZDWa+kEAA7AAAAAAAAAAAA'
     };
 
-    BttrLazyLoadingGlobal.retinaEnabled = false;
-
-    BttrLazyLoadingGlobal.prototype.setDefaultRanges = function(object) {
+    BttrLazyLoadingGlobal.prototype.setOptions = function(object) {
       if (object == null) {
         object = {};
       }
-      $.extend(this.constructor.ranges, object);
-      return this;
-    };
-
-    BttrLazyLoadingGlobal.prototype.setRetina = function(boolean) {
-      if (typeof boolean === 'boolean') {
-        this.constructor.retinaEnabled = boolean;
-      }
+      $.extend(true, this.constructor.options, object);
       return this;
     };
 
