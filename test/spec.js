2e3fd0f91c792b6d22686afcf1834295f4ff5074
shprink <julien.renaux@gmail.com>
2013-12-24 17:49:48 +0100

    adding event tests

--- test/spec.js
+++ test/spec.js
@@ -1,35 +1,35 @@
 var IMAGE_DIR = "../demo/img/";
 
 var testFixture = {
-	xs: {
-		src: IMAGE_DIR + "768x200.gif",
-		width: 768,
-		height: 200
+	xs : {
+		src : IMAGE_DIR + "768x200.gif",
+		width : 768,
+		height : 200
 	},
-	sm: {
-		src: IMAGE_DIR + "345x250.gif",
-		width: 345,
-		height: 250
+	sm : {
+		src : IMAGE_DIR + "345x250.gif",
+		width : 345,
+		height : 250
 	},
-	md: {
-		src: IMAGE_DIR + "455x350.gif",
-		width: 455,
-		height: 350
+	md : {
+		src : IMAGE_DIR + "455x350.gif",
+		width : 455,
+		height : 350
 	},
-	lg: {
-		src: IMAGE_DIR + "360x300.gif",
-		width: 360,
-		height: 300
+	lg : {
+		src : IMAGE_DIR + "360x300.gif",
+		width : 360,
+		height : 300
 	},
-	animation: "rotatedIn",
-	retina: true,
-	delay: 2000,
-	event: "mouseover",
-	container: "document.body",
-	triggerManually: true,
-	updateManually: true,
-	threshold: 500,
-	placeholder: "data:image/png;base64, iVBORw0KGgoAAAANSUhEUgAAAAUAAAAFCAYAAACNbyblAAAAHElEQVQI12P4//8/w38GIAXDIBKE0DHxgljNBAAO9TXL0Y4OHwAAAABJRU5ErkJggg=="
+	animation : "rotatedIn",
+	retina : true,
+	delay : 10,
+	event : "mouseover",
+	container : "document.body",
+	triggerManually : true,
+	updateManually : true,
+	threshold : 500,
+	placeholder : "data:image/png;base64, iVBORw0KGgoAAAANSUhEUgAAAAUAAAAFCAYAAACNbyblAAAAHElEQVQI12P4//8/w38GIAXDIBKE0DHxgljNBAAO9TXL0Y4OHwAAAABJRU5ErkJggg=="
 };
 
 var img1 = document.createElement("img");
@@ -51,8 +51,10 @@ img1.setAttribute("data-bttrlazyloading-retina", testFixture.retina);
 img1.setAttribute("data-bttrlazyloading-delay", testFixture.delay);
 img1.setAttribute("data-bttrlazyloading-event", testFixture.event);
 img1.setAttribute("data-bttrlazyloading-container", testFixture.container);
-img1.setAttribute("data-bttrlazyloading-triggermanually", testFixture.triggerManually);
-img1.setAttribute("data-bttrlazyloading-updatemanually", testFixture.updateManually);
+img1.setAttribute("data-bttrlazyloading-triggermanually",
+		testFixture.triggerManually);
+img1.setAttribute("data-bttrlazyloading-updatemanually",
+		testFixture.updateManually);
 img1.setAttribute("data-bttrlazyloading-threshold", testFixture.threshold);
 img1.setAttribute("data-bttrlazyloading-placeholder", testFixture.placeholder);
 
@@ -67,16 +69,42 @@ img4.setAttribute("id", "test4");
 
 var img5 = document.createElement("img");
 img5.setAttribute("id", "test5");
-img5.setAttribute("data-bttrlazyloading-xs", '{"src": "' + testFixture.xs.src + '", "width" : ' + testFixture.xs.width + ',  "height" : ' + testFixture.xs.height + '}');
-img5.setAttribute("data-bttrlazyloading-sm", '{"src": "' + testFixture.sm.src + '", "width" : ' + testFixture.sm.width + ',  "height" : ' + testFixture.sm.height + '}');
-img5.setAttribute("data-bttrlazyloading-md", '{"src": "' + testFixture.md.src + '", "width" : ' + testFixture.md.width + ',  "height" : ' + testFixture.md.height + '}');
-img5.setAttribute("data-bttrlazyloading-lg", '{"src": "' + testFixture.lg.src + '", "width" : ' + testFixture.lg.width + ',  "height" : ' + testFixture.lg.height + '}');
+img5.setAttribute("data-bttrlazyloading-xs", '{"src": "' + testFixture.xs.src
+		+ '", "width" : ' + testFixture.xs.width + ',  "height" : '
+		+ testFixture.xs.height + '}');
+img5.setAttribute("data-bttrlazyloading-sm", '{"src": "' + testFixture.sm.src
+		+ '", "width" : ' + testFixture.sm.width + ',  "height" : '
+		+ testFixture.sm.height + '}');
+img5.setAttribute("data-bttrlazyloading-md", '{"src": "' + testFixture.md.src
+		+ '", "width" : ' + testFixture.md.width + ',  "height" : '
+		+ testFixture.md.height + '}');
+img5.setAttribute("data-bttrlazyloading-lg", '{"src": "' + testFixture.lg.src
+		+ '", "width" : ' + testFixture.lg.width + ',  "height" : '
+		+ testFixture.lg.height + '}');
+
+var img6 = document.createElement("img");
+img6.setAttribute("id", "test6");
+img6.setAttribute("data-bttrlazyloading-xs", '{"src": "' + testFixture.xs.src
+		+ '", "width" : ' + testFixture.xs.width + ',  "height" : '
+		+ testFixture.xs.height + '}');
+img6.setAttribute("data-bttrlazyloading-sm", '{"src": "' + testFixture.sm.src
+		+ '", "width" : ' + testFixture.sm.width + ',  "height" : '
+		+ testFixture.sm.height + '}');
+img6.setAttribute("data-bttrlazyloading-md", '{"src": "' + testFixture.md.src
+		+ '", "width" : ' + testFixture.md.width + ',  "height" : '
+		+ testFixture.md.height + '}');
+img6.setAttribute("data-bttrlazyloading-lg", '{"src": "' + testFixture.lg.src
+		+ '", "width" : ' + testFixture.lg.width + ',  "height" : '
+		+ testFixture.lg.height + '}');
+img6.setAttribute("data-bttrlazyloading-triggermanually",
+		testFixture.triggerManually);
 
 window.document.body.appendChild(img1);
 window.document.body.appendChild(img2);
 window.document.body.appendChild(img3);
 window.document.body.appendChild(img4);
 window.document.body.appendChild(img5);
+window.document.body.appendChild(img6);
 
 describe("HTML5 data attribute", function() {
 	// LOCAL TESTS
@@ -100,7 +128,8 @@ describe("HTML5 data attribute", function() {
 		expect(obj.options.event).toEqual(testFixture.event);
 		expect(obj.options.container).toEqual(testFixture.container);
 		expect(obj.options.threshold).toEqual(testFixture.threshold);
-		expect(obj.options.triggermanually).toEqual(testFixture.triggerManually);
+		expect(obj.options.triggermanually)
+				.toEqual(testFixture.triggerManually);
 		expect(obj.options.updatemanually).toEqual(testFixture.updateManually);
 		expect(obj.options.placeholder).toEqual(testFixture.placeholder);
 	});
@@ -125,44 +154,44 @@ describe("HTML5 data attribute", function() {
 describe("jQuery Plugin", function() {
 	it("set the right options from instantiation", function() {
 		var obj4 = $("#test4").bttrlazyloading({
-			img: {
-				xs: {
-					src: testFixture.xs.src,
-					width: 111,
-					height: 222
+			img : {
+				xs : {
+					src : testFixture.xs.src,
+					width : 111,
+					height : 222
 				},
-				sm: {
-					src: testFixture.sm.src,
-					width: 333,
-					height: 444
+				sm : {
+					src : testFixture.sm.src,
+					width : 333,
+					height : 444
 				},
-				md: {
-					src: testFixture.md.src,
-					width: 555,
-					height: 666
+				md : {
+					src : testFixture.md.src,
+					width : 555,
+					height : 666
 				},
-				lg: {
-					src: testFixture.lg.src,
-					width: 777,
-					height: 888
+				lg : {
+					src : testFixture.lg.src,
+					width : 777,
+					height : 888
 				}
 			},
-			retina: true,
-			animation: 'fadeInUp',
-			delay: 1000,
-			event: 'click',
-			container: 'document.body',
-			threshold: 666,
-			placeholder: 'test',
-			triggermanually: true,
-			updatemanually: true,
-			onBeforeLoad: function($img, bttrLazyLoading) {
+			retina : true,
+			animation : 'fadeInUp',
+			delay : 1000,
+			event : 'click',
+			container : 'document.body',
+			threshold : 666,
+			placeholder : 'test',
+			triggermanually : true,
+			updatemanually : true,
+			onBeforeLoad : function($img, bttrLazyLoading) {
 				return true;
 			},
-			onAfterLoad: function($img, bttrLazyLoading) {
+			onAfterLoad : function($img, bttrLazyLoading) {
 				return true;
 			},
-			onError: function($img, bttrLazyLoading) {
+			onError : function($img, bttrLazyLoading) {
 				return true;
 			}
 		}).data('bttrlazyloading');
@@ -199,42 +228,42 @@ describe("jQuery Plugin", function() {
 	// GLOBAL TESTS
 	it("set the default global options", function() {
 		$.bttrlazyloading.setOptions({
-			img: {
-				xs: {
-					src: testFixture.xs.src,
-					width: 111,
-					height: 222
+			img : {
+				xs : {
+					src : testFixture.xs.src,
+					width : 111,
+					height : 222
 				},
-				sm: {
-					src: testFixture.sm.src,
-					width: 333,
-					height: 444
+				sm : {
+					src : testFixture.sm.src,
+					width : 333,
+					height : 444
 				},
-				md: {
-					src: testFixture.md.src,
-					width: 555,
-					height: 666
+				md : {
+					src : testFixture.md.src,
+					width : 555,
+					height : 666
 				},
-				lg: {
-					src: testFixture.lg.src,
-					width: 777,
-					height: 888
+				lg : {
+					src : testFixture.lg.src,
+					width : 777,
+					height : 888
 				}
 			},
-			retina: true,
-			animation: 'fadeInUp',
-			delay: 1000,
-			event: 'click',
-			container: 'document.body',
-			threshold: 666,
-			placeholder: 'test',
-			triggermanually: true,
-			updatemanually: true,
-			onBeforeLoad: function($img, bttrLazyLoading) {
+			retina : true,
+			animation : 'fadeInUp',
+			delay : 1000,
+			event : 'click',
+			container : 'document.body',
+			threshold : 666,
+			placeholder : 'test',
+			triggermanually : true,
+			updatemanually : true,
+			onBeforeLoad : function($img, bttrLazyLoading) {
 			},
-			onAfterLoad: function($img, bttrLazyLoading) {
+			onAfterLoad : function($img, bttrLazyLoading) {
 			},
-			onError: function($img, bttrLazyLoading) {
+			onError : function($img, bttrLazyLoading) {
 			}
 		});
 
@@ -268,10 +297,10 @@ describe("jQuery Plugin", function() {
 
 	it("set the default global ranges", function() {
 		$.bttrlazyloading.setRanges({
-			'xs': 5,
-			'sm': 6,
-			'md': 7,
-			'lg': 8
+			'xs' : 5,
+			'sm' : 6,
+			'md' : 7,
+			'lg' : 8
 		});
 		var obj3 = $("#test3").bttrlazyloading().data('bttrlazyloading');
 		expect(obj3.ranges.xs).toEqual(5);
@@ -291,35 +320,38 @@ describe("Responsivity", function() {
 });
 
 describe("Event", function() {
-	var testInstance = $("#test").bttrlazyloading().data('bttrlazyloading');
-	console.log(testInstance)
+	var testInstance = $("#test6").bttrlazyloading().data('bttrlazyloading');
+	var onbeforeLoad, onafterLoad;
 
-//	beforeEach(function(done) {
-//		setTimeout(function() {
-//			value = 0;
-//			done();
-//		}, 1);
+	beforeEach(function(done) {
+		onbeforeLoad = jasmine.createSpy("beforeLoadSpy");
+		onafterLoad = jasmine.createSpy("afterLoadSpy");
+		$("#test6").bind('bttrlazyloading.beforeLoad', onbeforeLoad);
+		$("#test6").bind('bttrlazyloading.afterLoad', onafterLoad);
+		$("#test6").trigger('bttrlazyloading.load');
+		setTimeout(function() {
+			done();
+		}, 1000);
+	});
+
+	it("should trigger bttrlazyloading.beforeLoad event", function(done) {
+		expect(onbeforeLoad).toHaveBeenCalled();
+		done();
+	});
+	it("should trigger bttrlazyloading.afterLoad event", function(done) {
+		expect(onafterLoad).toHaveBeenCalled();
+		done();
+	});
+//	it("should trigger bttrlazyloading.error event", function() {
+//
 //	});
-
-//	it("should trigger bttrlazyloading.beforeLoad event", function(done) {
-//		var onbeforeLoad = jasmine.createSpy("completeEventSpy");
-//		$("#test").bind('bttrlazyloading.beforeLoad', onbeforeLoad);
-//		$("#test").trigger('bttrlazyloading.load');
-//		expect(onbeforeLoad).toHaveBeenCalled();
+//	it("should trigger onBeforeLoad function", function() {
+//		// .calls.any()
 //	});
-	it("should trigger bttrlazyloading.afterLoad event", function() {
-
-	});
-	it("should trigger bttrlazyloading.error event", function() {
-
-	});
-	it("should trigger onBeforeLoad function", function() {
-		//.calls.any()
-	});
-	it("should trigger onAfterLoad function", function() {
-
-	});
-	it("should trigger onError function", function() {
-
-	});
+//	it("should trigger onAfterLoad function", function() {
+//
+//	});
+//	it("should trigger onError function", function() {
+//
+//	});
 });
